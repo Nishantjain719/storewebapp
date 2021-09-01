f70b96fb724df4ae2876381b26c12541bfebc6bf
Nishantjain719 <nishant.jain@asu.apeejay.edu>
2021-09-01 17:37:41 +0530

    create product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import "./App.css";
+import React, { useState } from "react";
+import Products from "./components/Products";
+import data from "./data.json";
 
 function App() {
+  const [products, setProducts] = useState(data.products);
+  const [size, setSize] = useState("");
+  const [sort, setSort] = useState("");
   return (
     <div className="grid-container">
       <header>
         <a href="/">Ecommerce store</a>
       </header>
-      <main></main>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Products products={products} />
+          </div>
+          <div className="sidebar"></div>
+        </div>
+      </main>
       <footer>All rights is reserved.</footer>
     </div>
   );
